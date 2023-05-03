90521f97287c924e26f8d636d32be66d6efe2703
jpmangone <jmangone@itba.edu.ar>
2023-05-03 18:51:04 -0300

    Entrega final Mangone VI

--- src/components/Item/Item.js
+++ src/components/Item/Item.js
@@ -4,10 +4,10 @@ import './Item.scss'
 const Item = ( {item} ) => {
 
     return (
-        <div className='anItem text-center col-lg-3 g-3'>
+        <div className='anItem col-lg-3 g-3'>
             <h2>{item.name}</h2>
             <img src={item.img}/>
-            <p>Precio: ${item.price}</p>
+            <p className="fw-semibold fst-italic">Precio: ${item.price}</p>
             <p><small>Categoría: {item.category}</small></p>
             <Link to={`/detail/${item.id}`} className='btn btn-dark'>Conoce más sobre este producto!</Link>
         </div>
--- src/components/Nosotros/Nosotros.js
+++ src/components/Nosotros/Nosotros.js
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react"
-
+import './Nosotros.scss'
 
 const Nosotros = () => {
 
@@ -11,6 +11,11 @@ const Nosotros = () => {
         <div onClick={() => handleClick("Hola mundo")} className="container my-5">
             <h2>Nosotros</h2>
             <hr/>
+            <div className="nosotrosPh">
+                <p>¡Hola a todos! Somos Agostina y Martina, dos hermanas que creamos Indra en el año 2020. Nuestra marca comenzó como una idea durante la pandemia, cuando nos dimos cuenta de que queríamos hacer algo significativo y creativo con nuestro tiempo en casa. Comenzamos con velas de soja 100% naturales y poco a poco fuimos ampliando nuestra gama de productos de home.</p>
+                <p>En Indra, nos esforzamos por ofrecer productos artesanales y ECO-friendly que no solo embellecen su hogar, sino que también promueven un estilo de vida saludable y sustentable. Nos enorgullece utilizar materiales renovables en todos nuestros productos para minimizar nuestro impacto en el planeta.</p> 
+                <p>¡Esperamos que nuestros productos mejoren su experiencia en el hogar y contribuyan a la construcción de un mundo más sustentable!</p> 
+            </div>
         </div>
     )
 }
